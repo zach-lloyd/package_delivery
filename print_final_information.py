00abc213be971d7e22b254feac377c72687d98c0
Zach Lloyd <joseph.z.lloyd@gmail.com>
2026-01-06 10:20:01 -0500

    add docstrings and a few additional comments to print_final_information() and user_interface()

--- print_final_information.py
+++ print_final_information.py
@@ -2,6 +2,14 @@ from convert_from_military_time import convert_from_military_time
 from check_packages import check_packages
 
 def print_final_information(packages, arrival_times, miles_travelled):
+    """
+    Prints the final results of running the algorithm.
+    
+    :param packages: A hash table with the packages that were delivered.
+    :param arrival_times: List of times, in military time, at which the trucks
+    arrived back at the Hub.
+    :param miles_travelled: The total miles travelled by all the trucks.
+    """
     print("\nWGUPS Routing Program\n")
     for i in range(len(arrival_times)):
         print(f"Truck {i + 1} Arrival, Miles Travelled: \
--- user_interface.py
+++ user_interface.py
@@ -1,6 +1,12 @@
 from convert_to_military_time import convert_to_military_time
 
 def user_interface(hash_table):
+    """
+    Runs the user interface, which allows users to specify a time and receive
+    a snapshot of each package's status at the specified time.
+    
+    :param hash_table: A hash table containing each of the packages.
+    """
     while True:
         try:
             user_time = input("Please enter a time to check status of package(s) "
@@ -9,6 +15,8 @@ def user_interface(hash_table):
             if user_time == "exit":
                 break
 
+            # Convert time to military time, because that is the format expected
+            # by each package's get_status_at_time() function
             converted_time = convert_to_military_time(user_time)
 
             print(f"\nStatus of all packages at {user_time}:")
@@ -19,15 +27,18 @@ def user_interface(hash_table):
             for id in range(1, 41):
                 package = hash_table.lookup(id)
                 package_status_at_time = package.get_status_at_time(converted_time)
+                # If a package hasn't been assigned to a truck (e.g if it is an
+                # overflow package) it is listed as "Unassigned"
                 t_id = "Unassigned"
 
                 if package.truck_id is not None:
                     t_id = package.truck_id
                 
-                # Print row
                 print(f"{package.ID:<4} {package_status_at_time:<12} \
                         {package.addr:<40} {package.deadline:<10} {t_id}")
-                
+
+        # Handle situation where the user does not enter the time in the expected
+        # format      
         except (ValueError, IndexError):
             print("Invalid entry. Please enter time in the format 'HH:MM AM/PM' "
                   "(e.g. 10:30 AM or 1:30 PM).")
